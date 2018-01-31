0e85006891b86a7904b9efa4183e17824eb1aacc
Marijana Vujkovic <vujkovicm@users.noreply.github.com>
2018-01-31 12:28:11 -0500

    Update 9_merge_all.R

--- 9_merge_all.R
+++ 9_merge_all.R
@@ -1,7 +1,7 @@
 library("reshape2")
 
 # import annotation
-anno = read.table("../output/region.anno.tab", T, sep = "\t", stringsAsFactors=F)
+anno = read.table("../output/region.anno.tab", T, sep = "\t", stringsAsFactors = F)
 
 # import counts
 mi = read.table("../output/counts_MI.hwe", T, stringsAsFactors = F)
@@ -21,16 +21,15 @@ colnames(hdl)[4:6] = c("HOM.HDL", "HET.HDL", "WT.HDL")
 colnames(tg)[4:6] = c("HOM.TG", "HET.TG", "WT.TG")
 colnames(tc)[4:6] = c("HOM.TC", "HET.TC", "WT.TC")
 
-
-mi = mi[which(mi$TEST %in% c("ALL", "AFF")),]
+mi = mi[which(mi$TEST %in% c("ALL", "AFF")), ]
 mi[mi$TEST == "AFF", "TEST"] = "MI"
-dm = dm[which(dm$TEST %in% c("AFF")),]
+dm = dm[which(dm$TEST %in% c("AFF")), ]
 dm$TEST = "DM"
 
 all = rbind(mi, dm)
 all = all[,c("SNP", "TEST", "GENO")]
 all.wide = reshape(all, idvar = "SNP", timevar = "TEST", direction = "wide")
-all.p = mi[which(mi$TEST == "ALL"), c("SNP","P")]
+all.p = mi[which(mi$TEST == "ALL"), c("SNP", "P")]
 colnames(all.p)[2] = "HWE.ALL"
 all.wide = merge(all.wide, all.p, by = "SNP")
 
@@ -41,22 +40,5 @@ all.wide = merge(all.wide, hdl[,c(2,4:6)], by = "SNP")
 
 final = merge(anno, all.wide, by.x = "Variant", by.y = "SNP")
 
-# fill in gene names
-for (i in 2:(nrow(final)-1))
-{
-        if (final$Gene[i] == "." & final$Gene[i-1] == final$Gene[i+1])
-        {
-                final$Gene[i] = final$Gene[i-1]
-        }
-        else if (final$Gene[i] == "." & final$Position[i] == final$Position[i+1])
-        {
-                final$Gene[i] = final$Gene[i+1]
-        }
-        else if (final$Gene[i] == "." & final$Position[i] == final$Position[i-1])
-        {
-                final$Gene[i] = final$Gene[i-1]
-        }
-}
-
-write.table(final, "../output/project_summary.tab", sep = "\t", row.names=F, col.names=T, quote = F)
+write.table(final, "../output/project_summary.tab", sep = "\t", row.names = F, col.names = T, quote = F)
 
